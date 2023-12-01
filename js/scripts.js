8182ace91fa566244ad078b0573bb4a3732dcd70
Taposh Sorkar <99810718+Sorkartaposh@users.noreply.github.com>
2023-12-02 00:30:07 +0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,25 @@
 		});	
 
 		// custom cursor
+		var kursorx = new kursor({
+			type:4,
+			color: 'rgba(255,255,255)',
+		})
 
+		// function mouseenter () {
+		// 	kursorx.hidden()
+		// }
+		// function mouseleave () {
+		// 	kursorx.hidden(false)
+		// }
+		
+		// var kursor2 = new kursor({
+		// 	el: '.caja',
+		// 	color: 'rgba(0,0,255)',
+		// 	type: 4,
+		// })
 
-
+		
 
 		
 		
@@ -38,3 +54,39 @@
 		
 	});
 })(jQuery);
+
+	// page loader
+	$(window).on('load',function(){
+		setTimeout(function(){ 
+		$('.page-loader').fadeOut('slow');
+		},1000);
+	});
+
+	// disbale code inspect
+	// take body to change the content
+	const body = document.getElementsByTagName('body');
+	// stop keyboard shortcuts
+	window.addEventListener("keydown", (event) => {
+	if(event.ctrlKey && (event.key === "S" || event.key === "s")) {
+		event.preventDefault();
+	}
+	if(event.ctrlKey && (event.key === "C")) {
+		event.preventDefault();
+	}
+	if(event.ctrlKey && (event.key === "E" || event.key === "e")) {
+		event.preventDefault();
+	}
+	if(event.ctrlKey && (event.key === "I" || event.key === "i")) {
+		event.preventDefault();
+	}
+	if(event.ctrlKey && (event.key === "K" || event.key === "k")) {
+		event.preventDefault();
+	}
+	if(event.ctrlKey && (event.key === "U" || event.key === "u")) {
+		event.preventDefault();
+	}
+	});
+	// stop right click
+	document.addEventListener('contextmenu', function(e) {
+	e.preventDefault();
+	});
